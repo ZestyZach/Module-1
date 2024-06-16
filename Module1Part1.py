42505be31df5639c099012aabbd1dce11766c5a6
ZestyZach <zachary.williamson@csuglobal.edu>
2024-06-16 14:56:32 -0400

    Add files via upload

--- Module1Part1.py
+++ Module1Part1.py
@@ -1,5 +1,14 @@
+#user inputs 2 numbers
 num1 = int(input("Enter First Number:\n"))
 num2 = int(input("Enter Second Number:\n"))
-print(num1,"+",num2,"is",(num1 + num2))
-print(num1,"-",num2,"is",(num1 - num2))
+
+#add numbers
+sum = int(num1 + num2)
+
+#subtract numbers
+diff = int(num1 - num2)
+
+#output results
+print(num1,"+",num2,"is",sum)
+print(num1,"-",num2,"is",diff)
 input("Press Enter to exit.")
--- Module1Part2.py
+++ Module1Part2.py
@@ -1,5 +1,17 @@
+#user inputs 2 numbers
 num1 = int(input("Enter First Number\n"))
 num2 = int(input("Enter Second Number\n"))
-print(num1,"*",num2,"is",(num1 * num2))
-print(num1,"/",num2,"is",(num1 / num2))
+
+#multiply numbers
+mult = num1 * num2
+
+#make sure num2 is not 0 before dividing
+if num2 != 0:
+    div = num1 / num2
+else:
+    div = "undefined"
+
+#output results
+print(num1,"*",num2,"is",mult)
+print(num1,"/",num2,"is",div)
 input("Press Enter to exit.")
